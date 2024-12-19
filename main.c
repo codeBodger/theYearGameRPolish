6520f1ca0ba3e2fc7bfb5d6308ffe8b6a4c93202
codeBodger <diesel.ack+github@gmail.com>
2024-12-19 10:23:49 -0500

    Minimal mainloop, Minimal general logic functions

--- main.c
+++ main.c
@@ -1,5 +1,34 @@
-#include "funcs.h"
+#include "main.h"
+
+uint64_t i;
 
 int main() {
+    for (i = 0; ; i++) {
+        digitOrder();
+    }
     return 0;
+}
+
+void digitOrder() {
+
+}
+
+void holPattern(int d1, int d2, int d3, int d4) {
+
+}
+
+void binOpOrd3h(int n1, int n2, int n3, int n4) {
+
+}
+
+void binOpOrd2h(int n1, int n2, int n3) {
+
+}
+
+void binOpOrd1h(int n1, int n2) {
+
+}
+
+void binOpOrd0h(int n1) {
+
 }
---
+++ main.h
@@ -0,0 +1,8 @@
+#include "funcs.h"
+
+void digitOrder();
+void holPattern(int d1, int d2, int d3, int d4);
+void binOpOrd3h(int n1, int n2, int n3, int n4);
+void binOpOrd2h(int n1, int n2, int n3);
+void binOpOrd1h(int n1, int n2);
+void binOpOrd0h(int n1);
