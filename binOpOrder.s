137355257d581e0906bed2c6aed9482385353ad5
codeBodger <diesel.ack+github@gmail.com>
2024-12-23 14:58:08 -0500

    Smart incrementing!

--- binOpOrder.s
+++ binOpOrder.s
@@ -5,6 +5,8 @@ binOpOrd0h:
     # back up the link register
     mov binOpOrderLR, lr
 
+    mov incshft, #-1
+
     # 0
         mov bosi, i
         # get a: nothing to do, it's already there
@@ -12,9 +14,10 @@ binOpOrd0h:
         adr bojt, binOpOrd0h_0_getu_0_jt
             # load the address of the start of the jump table
         binOpOrd0h_0_getu_0_start:
-            and bosw, bosi, #0xC000000000000000
+            and bosw, bosi, #0xC000000000000000 // 3 from left
             lsr bosw, bosw, #59
             lsl bosi, bosi, #2
+            add incshft, incshft, #2
             add bosw, bosw, bojt
             br bosw
         binOpOrd0h_0_getu_0_jt:
@@ -32,7 +35,7 @@ binOpOrd0h:
 
             # case 3:
             bl dfact
-            
+
         binOpOrd0h_0_getu_0_btm:
             cmp r0i, #0
             beq binOpOrd0h_0_end
@@ -40,13 +43,14 @@ binOpOrd0h:
         binOpOrd0h_0_getu_0_end:
 
         # get sign
-            and bosw, bosi, #0x8000000000000000
+            and bosw, bosi, #0x8000000000000000 // 1 from left
             lsl bosi, bosi, #1
+            add incshft, incshft, #1
             cmp bosw, #0
             beq binOpOrd0h_0_sign_0_end
             sub r0i, wzr, r0i
         binOpOrd0h_0_sign_0_end:
-        
+
         # check if in range
             cmp r0i, #0
             blt binOpOrd0h_0_end
@@ -60,6 +64,9 @@ binOpOrd0h:
             str i, [vals, r0l, LSL #3]
 
     binOpOrd0h_0_end:
+        # maxshft = max(incshft, maxshft)
+        cmp incshft, maxshft
+        csel maxshft, incshft, maxshft, GT
 
     # restore the link register
     mov lr, binOpOrderLR
