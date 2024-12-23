d15dd0414087f06025454120b49746995e727b37
codeBodger <diesel.ack+github@gmail.com>
2024-12-23 15:49:34 -0500

    Macro for GET_UNARIES

--- binOpOrder.s
+++ binOpOrder.s
@@ -1,5 +1,40 @@
 .include "reg_rename.has"
 
+
+.macro GET_UNARIES, on0ret
+adr bojt, getu_0_jt\@
+    # load the address of the start of the jump table
+getu_0_start\@:
+    and bosw, bosi, #0xC000000000000000 // 3 from left
+    lsr bosw, bosw, #59
+    lsl bosi, bosi, #2
+    add incshft, incshft, #2
+    add bosw, bosw, bojt
+    br bosw
+getu_0_jt\@:
+    # case 0:
+    b getu_0_end\@
+    nop
+
+    # case 1:
+    bl isqrt
+    b getu_0_btm\@
+
+    # case 2:
+    bl fact
+    b getu_0_btm\@
+
+    # case 3:
+    bl dfact
+
+getu_0_btm\@:
+    cmp r0i, #0
+    beq on0ret
+    b getu_0_start\@
+getu_0_end\@:
+.endm
+
+
     .global binOpOrd0h
 binOpOrd0h:
     # back up the link register
@@ -11,36 +46,7 @@ binOpOrd0h:
         mov bosi, i
         # get a: nothing to do, it's already there
         # get unaries
-        adr bojt, binOpOrd0h_0_getu_0_jt
-            # load the address of the start of the jump table
-        binOpOrd0h_0_getu_0_start:
-            and bosw, bosi, #0xC000000000000000 // 3 from left
-            lsr bosw, bosw, #59
-            lsl bosi, bosi, #2
-            add incshft, incshft, #2
-            add bosw, bosw, bojt
-            br bosw
-        binOpOrd0h_0_getu_0_jt:
-            # case 0:
-            b binOpOrd0h_0_getu_0_end
-            nop
-
-            # case 1:
-            bl isqrt
-            b binOpOrd0h_0_getu_0_btm
-
-            # case 2:
-            bl fact
-            b binOpOrd0h_0_getu_0_btm
-
-            # case 3:
-            bl dfact
-
-        binOpOrd0h_0_getu_0_btm:
-            cmp r0i, #0
-            beq binOpOrd0h_0_end
-            b binOpOrd0h_0_getu_0_start
-        binOpOrd0h_0_getu_0_end:
+        GET_UNARIES binOpOrd0h_0_end
 
         # get sign
             and bosw, bosi, #0x8000000000000000 // 1 from left
