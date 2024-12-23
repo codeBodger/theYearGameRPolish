74ae9f57e234217552ded268b829c5523e9f64ec
codeBodger <diesel.ack+github@gmail.com>
2024-12-23 11:27:06 -0500

    Massive MAINLOOP macro calling HOLE_PATTERN_NNN macros

--- main.c
+++ main.c
@@ -25,109 +25,120 @@ int main() {
 
     signal(SIGINT, interuptHandler);
 
-    __asm__(""
-        "# do {\n\t"
-        "mov i, #0\n"
-    "mainloopstart:\n\t"
-        "mov score, #3\n"
-    ); {
+    __asm__("mov score, #2\n");
+    MAINLOOP(000);
+    MAINLOOP(001);
+    MAINLOOP(010);
+    MAINLOOP(011);
+    MAINLOOP(100);
+    MAINLOOP(101);
+    MAINLOOP(110);
+    __asm__("mov score, #3\n");
+    MAINLOOP(111);
 
-    // digitOrder:
-        holPattern(DIGIT1, DIGIT2, DIGIT3, DIGIT4);
-        __asm__("mov score, #2\n");
-        holPattern(DIGIT1, DIGIT2, DIGIT4, DIGIT3);
-        holPattern(DIGIT1, DIGIT3, DIGIT2, DIGIT4);
-        // holPattern(DIGIT1, DIGIT4, DIGIT2, DIGIT3);
-        // holPattern(DIGIT1, DIGIT4, DIGIT3, DIGIT2);
-        // holPattern(DIGIT1, DIGIT3, DIGIT4, DIGIT2);
+    // __asm__(""
+    //     "# do {\n\t"
+    //     "mov i, #0\n"
+    // "mainloopstart:\n\t"
+    //     "mov score, #3\n"
+    // ); {
 
-        holPattern(DIGIT2, DIGIT1, DIGIT3, DIGIT4);
-        holPattern(DIGIT2, DIGIT1, DIGIT4, DIGIT3);
-        holPattern(DIGIT3, DIGIT1, DIGIT2, DIGIT4);
-        // holPattern(DIGIT4, DIGIT1, DIGIT2, DIGIT3);
-        // holPattern(DIGIT4, DIGIT1, DIGIT3, DIGIT2);
-        // holPattern(DIGIT3, DIGIT1, DIGIT4, DIGIT2);
+    // // digitOrder:
+    //     holPattern(DIGIT1, DIGIT2, DIGIT3, DIGIT4);
+    //     __asm__("mov score, #2\n");
+    //     holPattern(DIGIT1, DIGIT2, DIGIT4, DIGIT3);
+    //     holPattern(DIGIT1, DIGIT3, DIGIT2, DIGIT4);
+    //     // holPattern(DIGIT1, DIGIT4, DIGIT2, DIGIT3);
+    //     // holPattern(DIGIT1, DIGIT4, DIGIT3, DIGIT2);
+    //     // holPattern(DIGIT1, DIGIT3, DIGIT4, DIGIT2);
 
-        holPattern(DIGIT3, DIGIT2, DIGIT1, DIGIT4);
-        // holPattern(DIGIT4, DIGIT2, DIGIT1, DIGIT3);
-        holPattern(DIGIT2, DIGIT3, DIGIT1, DIGIT4);
-        holPattern(DIGIT2, DIGIT4, DIGIT1, DIGIT3);
-        // holPattern(DIGIT3, DIGIT4, DIGIT1, DIGIT2);
-        // holPattern(DIGIT4, DIGIT3, DIGIT1, DIGIT2);
+    //     holPattern(DIGIT2, DIGIT1, DIGIT3, DIGIT4);
+    //     holPattern(DIGIT2, DIGIT1, DIGIT4, DIGIT3);
+    //     holPattern(DIGIT3, DIGIT1, DIGIT2, DIGIT4);
+    //     // holPattern(DIGIT4, DIGIT1, DIGIT2, DIGIT3);
+    //     // holPattern(DIGIT4, DIGIT1, DIGIT3, DIGIT2);
+    //     // holPattern(DIGIT3, DIGIT1, DIGIT4, DIGIT2);
 
-        // holPattern(DIGIT4, DIGIT2, DIGIT3, DIGIT1);
-        holPattern(DIGIT3, DIGIT2, DIGIT4, DIGIT1);
-        // holPattern(DIGIT4, DIGIT3, DIGIT2, DIGIT1);
-        // holPattern(DIGIT3, DIGIT4, DIGIT2, DIGIT1);
-        holPattern(DIGIT2, DIGIT4, DIGIT3, DIGIT1);
-        holPattern(DIGIT2, DIGIT3, DIGIT4, DIGIT1);
+    //     holPattern(DIGIT3, DIGIT2, DIGIT1, DIGIT4);
+    //     // holPattern(DIGIT4, DIGIT2, DIGIT1, DIGIT3);
+    //     holPattern(DIGIT2, DIGIT3, DIGIT1, DIGIT4);
+    //     holPattern(DIGIT2, DIGIT4, DIGIT1, DIGIT3);
+    //     // holPattern(DIGIT3, DIGIT4, DIGIT1, DIGIT2);
+    //     // holPattern(DIGIT4, DIGIT3, DIGIT1, DIGIT2);
 
-    } __asm__(""
-        "# i++\n\t"
-        "add i, i, #1\n\t"
-        "# } while (i)\n\t"
-        "cmp i, #0\n\t"
-        "bne mainloopstart\n"
-    );
+    //     // holPattern(DIGIT4, DIGIT2, DIGIT3, DIGIT1);
+    //     holPattern(DIGIT3, DIGIT2, DIGIT4, DIGIT1);
+    //     // holPattern(DIGIT4, DIGIT3, DIGIT2, DIGIT1);
+    //     // holPattern(DIGIT3, DIGIT4, DIGIT2, DIGIT1);
+    //     holPattern(DIGIT2, DIGIT4, DIGIT3, DIGIT1);
+    //     holPattern(DIGIT2, DIGIT3, DIGIT4, DIGIT1);
+
+    // } __asm__(""
+    //     "# i++\n\t"
+    //     "add i, i, #1\n\t"
+    //     "# } while (i)\n\t"
+    //     "cmp i, #0\n\t"
+    //     "bne mainloopstart\n"
+    // );
 
     // for (i = 0; ; i++) {
     //     goto digitOrder;
     // mainloopend:
     // }
 
-    // return 0;
-    __asm__("# return 0;\n\t"
-        "# Tricking it into including stuff after returning\n\t"
-        "mov w0, #0\n\t"
-        "ldp x29, x30, [sp], 16\n\t"
-        // ".cfi_restore 30\n\t"
-        // ".cfi_restore 29\n\t"
-        // ".cfi_def_cfa_offset 0\n\t"
-        "ret\n"
-        // ".cfi_endproc\n"
-    );
+    return 0;
+    // __asm__("# return 0;\n\t"
+    //     "# Tricking it into including stuff after returning\n\t"
+    //     "mov w0, #0\n\t"
+    //     "ldp x29, x30, [sp], 16\n\t"
+    //     // ".cfi_restore 30\n\t"
+    //     // ".cfi_restore 29\n\t"
+    //     // ".cfi_def_cfa_offset 0\n\t"
+    //     "ret\n"
+    //     // ".cfi_endproc\n"
+    // );
 
 
-    __asm__(""
-        "# MAKE SURE TO FIX REGISTER NUMBERS HERE!!!\n\t"
-        "# THAT IS: w19:w22 -> hp0i:hp3i\n"
-    "holPattern:\n\t"
-        "# back up the link register\n\t"
-        "mov holPatternLR, lr\n\n\t"
+    // __asm__(""
+    //     "# MAKE SURE TO FIX REGISTER NUMBERS HERE!!!\n\t"
+    //     "# THAT IS: w19:w22 -> hp0i:hp3i\n"
+    // "holPattern:\n\t"
+    //     "# back up the link register\n\t"
+    //     "mov holPatternLR, lr\n\n\t"
 
-        "# copy params to stable registers for this function\n\t"
-        "mov hp0i, p0i\n\t"
-        "mov hp1i, p1i\n\t"
-        "mov hp2i, p2i\n\t"
-        "mov hp3i, p3i\n\n"
-    );
+    //     "# copy params to stable registers for this function\n\t"
+    //     "mov hp0i, p0i\n\t"
+    //     "mov hp1i, p1i\n\t"
+    //     "mov hp2i, p2i\n\t"
+    //     "mov hp3i, p3i\n\n"
+    // );
 
-        register int d1, d2, d3, d4;
-        __asm__("sub score, score, #1\n");
-        // 000
-        binOpOrd0h(((d1*10 +d2)*10 +d3)*10 +d4);
-        // 001
-        binOpOrd1h((d1*10 +d2)*10 +d3, d4);
-        // 010
-        binOpOrd1h(d1*10 +d2, d3*10 +d4);
-        // 011
-        binOpOrd2h(d1*10 +d2, d3, d4);
-        // 100
-        binOpOrd1h(d1, (d2*10 +d3)*10 +d4);
-        // 101
-        binOpOrd2h(d1, d2*10 +d3, d4);
-        // 110
-        binOpOrd2h(d1, d2, d3*10 +d4);
-        __asm__("add score, score, #1\n");
-        // 111
-        binOpOrd3h(d1, d2, d3, d4);
+    //     register int d1, d2, d3, d4;
+    //     __asm__("sub score, score, #1\n");
+    //     // 000
+    //     binOpOrd0h(((d1*10 +d2)*10 +d3)*10 +d4);
+    //     // 001
+    //     binOpOrd1h((d1*10 +d2)*10 +d3, d4);
+    //     // 010
+    //     binOpOrd1h(d1*10 +d2, d3*10 +d4);
+    //     // 011
+    //     binOpOrd2h(d1*10 +d2, d3, d4);
+    //     // 100
+    //     binOpOrd1h(d1, (d2*10 +d3)*10 +d4);
+    //     // 101
+    //     binOpOrd2h(d1, d2*10 +d3, d4);
+    //     // 110
+    //     binOpOrd2h(d1, d2, d3*10 +d4);
+    //     __asm__("add score, score, #1\n");
+    //     // 111
+    //     binOpOrd3h(d1, d2, d3, d4);
 
-    __asm__(""
-        "# restore the link register\n\t"
-        "mov lr, holPatternLR\n\n"
+    // __asm__(""
+    //     "# restore the link register\n\t"
+    //     "mov lr, holPatternLR\n\n"
         
-        "ret\n\n"
-    );
+    //     "ret\n\n"
+    // );
 }
 
 void binOpOrd3h(int n1, int n2, int n3, int n4) {
--- main.h
+++ main.h
@@ -11,8 +11,72 @@
 #define LSR(args) __asm__("lsr " args "\n")
 #define AND(args) __asm__("and " args "\n")
 
+
+#define MAINLOOP(holePatn) __asm__("" \
+        "# do {\n\t" \
+        "mov i, #0\n" \
+    "mainloopstart:\n\t" \
+        "mov score, #3\n" \
+    ); { \
+
+    /* digitOrder: */ \
+        HOLE_PATTERN_##holePatn(DIGIT1, DIGIT2, DIGIT3, DIGIT4); \
+        __asm__("sub score, score, #1\n"); \
+        HOLE_PATTERN_##holePatn(DIGIT1, DIGIT2, DIGIT4, DIGIT3); \
+        HOLE_PATTERN_##holePatn(DIGIT1, DIGIT3, DIGIT2, DIGIT4); \
+        /* HOLE_PATTERN_##holePatn(DIGIT1, DIGIT4, DIGIT2, DIGIT3); \
+        HOLE_PATTERN_##holePatn(DIGIT1, DIGIT4, DIGIT3, DIGIT2); \
+        HOLE_PATTERN_##holePatn(DIGIT1, DIGIT3, DIGIT4, DIGIT2); */ \
+
+        HOLE_PATTERN_##holePatn(DIGIT2, DIGIT1, DIGIT3, DIGIT4); \
+        HOLE_PATTERN_##holePatn(DIGIT2, DIGIT1, DIGIT4, DIGIT3); \
+        HOLE_PATTERN_##holePatn(DIGIT3, DIGIT1, DIGIT2, DIGIT4); \
+        /* HOLE_PATTERN_##holePatn(DIGIT4, DIGIT1, DIGIT2, DIGIT3); \
+        HOLE_PATTERN_##holePatn(DIGIT4, DIGIT1, DIGIT3, DIGIT2); \
+        HOLE_PATTERN_##holePatn(DIGIT3, DIGIT1, DIGIT4, DIGIT2); */ \
+
+        HOLE_PATTERN_##holePatn(DIGIT3, DIGIT2, DIGIT1, DIGIT4); \
+        /* HOLE_PATTERN_##holePatn(DIGIT4, DIGIT2, DIGIT1, DIGIT3); */ \
+        HOLE_PATTERN_##holePatn(DIGIT2, DIGIT3, DIGIT1, DIGIT4); \
+        HOLE_PATTERN_##holePatn(DIGIT2, DIGIT4, DIGIT1, DIGIT3); \
+        /* HOLE_PATTERN_##holePatn(DIGIT3, DIGIT4, DIGIT1, DIGIT2); \
+        HOLE_PATTERN_##holePatn(DIGIT4, DIGIT3, DIGIT1, DIGIT2); */ \
+
+        /* HOLE_PATTERN_##holePatn(DIGIT4, DIGIT2, DIGIT3, DIGIT1); */ \
+        HOLE_PATTERN_##holePatn(DIGIT3, DIGIT2, DIGIT4, DIGIT1); \
+        /* HOLE_PATTERN_##holePatn(DIGIT4, DIGIT3, DIGIT2, DIGIT1); \
+        HOLE_PATTERN_##holePatn(DIGIT3, DIGIT4, DIGIT2, DIGIT1); */ \
+        HOLE_PATTERN_##holePatn(DIGIT2, DIGIT4, DIGIT3, DIGIT1); \
+        HOLE_PATTERN_##holePatn(DIGIT2, DIGIT3, DIGIT4, DIGIT1); \
+        __asm__("add score, score, #1\n")
+
+    } __asm__("" \
+        "# i++\n\t" \
+        "add i, i, #1\n\t" \
+        "# } while (i)\n\t" \
+        "cmp i, #0\n\t" \
+        "bne mainloopstart\n" \
+    );
+
+    // for (i = 0; ; i++) {
+    //     goto digitOrder;
+    // mainloopend:
+    // }
+
+// #define HOLE_PATTERN(d1, d2, d3, d4) HOLE_PATTERN_##holePatn(d1, d2, d3, d4)
+
+#define HOLE_PATTERN_000 binOpOrd0h(((d1*10 +d2)*10 +d3)*10 +d4)
+#define HOLE_PATTERN_001 binOpOrd1h((d1*10 +d2)*10 +d3, d4)
+#define HOLE_PATTERN_010 binOpOrd1h(d1*10 +d2, d3*10 +d4)
+#define HOLE_PATTERN_011 binOpOrd2h(d1*10 +d2, d3, d4)
+#define HOLE_PATTERN_100 binOpOrd1h(d1, (d2*10 +d3)*10 +d4)
+#define HOLE_PATTERN_101 binOpOrd2h(d1, d2*10 +d3, d4)
+#define HOLE_PATTERN_110 binOpOrd2h(d1, d2, d3*10 +d4)
+#define HOLE_PATTERN_111 binOpOrd3h(d1, d2, d3, d4)
+
+
 // void digitOrder();
-void holPattern(int d1, int d2, int d3, int d4);
+// void holPattern(int d1, int d2, int d3, int d4);
 void binOpOrd3h(int n1, int n2, int n3, int n4);
 void binOpOrd2h(int n1, int n2, int n3);
 void binOpOrd1h(int n1, int n2);
