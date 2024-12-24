8848e4424c97f1e18e0295c0d64ee332a608b2f9
codeBodger <diesel.ack+github@gmail.com>
2024-12-24 13:11:10 -0500

    interuptHandler doesn't really do anything anymore: remove, but double-label handler

--- signalHandler.s
+++ signalHandler.s
@@ -7,48 +7,6 @@ handlerLoopPrintString:
     .string "%3d: value:%16lx\tscore:%d\t\t%3d: value:%16lx\tscore:%d\n"
 
 .text
-    .global interuptHandler
-interuptHandler:
-    // Save ALL registers
-    stp  x0,  x1, [SP, #-16]!
-    stp  x2,  x3, [SP, #-16]!
-    stp  x4,  x5, [SP, #-16]!
-    stp  x6,  x7, [SP, #-16]!
-    stp  x8,  x9, [SP, #-16]!
-    stp x10, x11, [SP, #-16]!
-    stp x12, x13, [SP, #-16]!
-    stp x14, x15, [SP, #-16]!
-    stp x16, x17, [SP, #-16]!
-    stp x18, x19, [SP, #-16]!
-    stp x20, x21, [SP, #-16]!
-    stp x22, x23, [SP, #-16]!
-    stp x24, x25, [SP, #-16]!
-    stp x26, x27, [SP, #-16]!
-    stp x28, x29, [SP, #-16]!
-    stp lr,  xzr, [SP, #-16]!
-
-    bl handler
-
-    // Restore ALL registers
-    ldp lr,  xzr, [SP], #16
-    ldp x28, x29, [SP], #16
-    ldp x26, x27, [SP], #16
-    ldp x24, x25, [SP], #16
-    ldp x22, x23, [SP], #16
-    ldp x20, x21, [SP], #16
-    ldp x18, x19, [SP], #16
-    ldp x16, x17, [SP], #16
-    ldp x14, x15, [SP], #16
-    ldp x12, x13, [SP], #16
-    ldp x10, x11, [SP], #16
-    ldp  x8,  x9, [SP], #16
-    ldp  x6,  x7, [SP], #16
-    ldp  x4,  x5, [SP], #16
-    ldp  x2,  x3, [SP], #16
-    ldp  x0,  x1, [SP], #16
-
-    ret
-
     .global quitHandler
 quitHandler:
     bl handler
@@ -60,6 +18,8 @@ earlyExit:
     mov x8, #94
     svc #0
 
+    .global interuptHandler
+interuptHandler:
     .global handler
 handler:
     // Save ALL registers
